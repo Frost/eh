6ef8ac8d0d95fd28c9f04f354510ce0cde32e24b
Martin Frost <frost@ceri.se>
2014-11-30 17:19:02 +0100

    Hexify mix.exs file
    * Remove excess functions
    * Add description and package definitions

--- mix.exs
+++ mix.exs
@@ -3,16 +3,28 @@ defmodule Eh.Mixfile do
 
   def project do
     [app: :eh,
-     version: "0.0.1",
-     elixir: "~> 1.1-dev",
+     version: "0.1.0",
+     elixir: "~> 1.0.0",
      escript: [main_module: Eh],
+     description: description,
+     package: package,
      deps: []]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    []
+  end
+
+  defp description do
+    """
+    Lookup Elixir documentation from the command line
+    """
+  end
+
+  defp package do
+    [ files: ~w[ lib README.md mix.exs LICENCE ],
+      contributors: ["Martin Frost"],
+      licenses: ["Apache 2.0"],
+      links: %{ "GitHub" => "https://github.com/Frost/eh.git" } ]
   end
 end
