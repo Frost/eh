b1818e4825ca90775173d5f23ad541620773e39d
Martin Frost <frost@ceri.se>
2015-02-26 19:15:17 +0100

    version 0.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eh.Mixfile do
 
   def project do
     [app: :eh,
-     version: "0.1.1",
+     version: "0.1.2",
      elixir: "~> 1.0",
      escript: [main_module: Eh],
      description: description,
